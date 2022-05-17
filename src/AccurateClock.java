5c025c81b7ec896a5521f547e588bd18cf0d5090
idancoh30 <102730179+idancoh30@users.noreply.github.com>
2022-05-17 15:35:31 +0300

    Initial commit

---
+++ src/AccurateClock.java
@@ -0,0 +1,39 @@
+public class AccurateClock extends Clock {
+    private int seconds;
+
+    public AccurateClock(int hours, int minutes, int seconds) {
+        super(hours, minutes);
+        if (seconds < 0 || seconds > 59) {
+            this.seconds = 0;
+        } else {
+            this.seconds = seconds;
+        }
+    }
+    public AccurateClock(){
+        super();
+        this.seconds=0;
+    }
+
+    @Override
+    public String toString(){
+        if((seconds - 10) < 0)
+            return super.toString() + ":0"+seconds;
+        return super.toString() + ":"+seconds;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof AccurateClock)) {
+            return false;
+        }
+        AccurateClock otherClock = (AccurateClock) other;
+        return super.equals(otherClock) && this.seconds == otherClock.seconds;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode()*60+seconds;
+    }
+
+}
+
---
+++ src/Clock.java
@@ -0,0 +1,62 @@
+import java.util.Objects;
+
+public class Clock {
+    private int hours, minutes;
+
+    public Clock(int hours, int minutes) {
+        if (hours < 0 || hours > 23)
+            this.hours = 0;
+        if (minutes < 0 || minutes > 59)
+            this.minutes = 0;
+        else {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+    }
+
+    public Clock() {
+        this.hours = 0;
+        this.minutes = 0;
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        if ((hours - 10) < 0) {
+            output += "0" + hours + ":";
+        } else {
+            output += hours + ":";
+        }
+        if ((minutes - 10) < 0) {
+            output += "0" + minutes;
+        } else {
+            output += minutes;
+        }
+        return output;
+    }
+
+    @Override
+    public boolean equalsss(Object other) {
+        if (!(other instanceof Clock)) {
+            return false;
+        }
+        Clock otherClock = (Clock) other;
+        return this.hours == otherClock.hours && this.minutes == otherClock.minutes;
+    }
+
+    @Override
+    public boolean equals(Object other)
+    {
+        if(other instanceof Clock)
+        {
+            Clock otherClock = (Clock) other;
+            return this.hours == otherClock.hours && this.minutes == otherClock.minutes;
+        }
+        return false;
+    }
+    // Count minutes passed from 00:00
+    @Override
+    public int hashCode() {
+        return 60 * hours + minutes;
+    }
+}
