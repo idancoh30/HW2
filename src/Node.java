a7ce5e26254d6a0028bc3eae74c43d169ab95a0f
idancoh30 <102730179+idancoh30@users.noreply.github.com>
2022-05-23 22:15:34 +0300

    with tiudddd

--- src/Node.java
+++ src/Node.java
@@ -1,28 +1,57 @@
+/**
+ * Represents a single generic node. Each node contains value and pointer to next node.
+ * @param <E> type parameter for generic node.
+ */
 public class Node<E> {
     private E value;
     private Node<E> next;
 
+    /**
+     * Constructor. Initializes a new node which points to another node.
+     * @param value - node's value.
+     * @param next - pointer to next node.
+     */
     public Node(E value, Node<E> next) {
         this.value = value;
         this.next = next;
     }
 
+    /**
+     * Constructor. Initializes a new node which points to null.
+     * @param value - node's value.
+     */
     public Node(E value) {
         this(value, null);
     }
 
+    /**
+     * Returns node's value.
+     * @return node's value.
+     */
     public E getValue() {
         return value;
     }
 
+    /**
+     * Returns reference to the next node.
+     * @return reference to the next node.
+     */
     public Node<E> getNext() {
         return next;
     }
 
+    /**
+     * Sets node's next reference. value.
+     * @param value - the value to be set into current node.
+     */
     public void setValue(E value) {
         this.value = value;
     }
 
+    /**
+     * Sets reference to the next node.
+     * @param next - next node reference.
+     */
     public void setNext(Node<E> next) {
         this.next = next;
     }
--- src/Stack.java
+++ src/Stack.java
@@ -1,16 +1,31 @@
+/**
+ * Represents a generic LIFO (=last in first out) stack.
+ * @param <E> type parameter for generic stack.
+ */
 public class Stack<E> {
     private Node<E> head;
 
+    /**
+     * Constructor. Initializes a new empty stack.
+     */
     public Stack() {
         head = null;
     }
 
+    /**
+     * Pushes new element into stack's buttom.
+     * @param item - element to be pushed.
+     */
     public void push(E item) {
         Node<E> newHead = new Node<>(item);
         newHead.setNext(head);
         head = newHead;
     }
 
+    /**
+     * Pop out the last element was pushed into stack.
+     * @return top most element in stack.
+     */
     public E pop() {
         if (head == null) {
             return null;
@@ -20,6 +35,10 @@ public class Stack<E> {
         return item;
     }
 
+    /**
+     * Shows the head of the stack without pushing it out.
+     * @return top most element in stack.
+     */
     public E peek() {
         if (head == null) {
             return null;
@@ -27,6 +46,10 @@ public class Stack<E> {
         return head.getValue();
     }
 
+    /**
+     * Checks if stack is empty.
+     * @return true if stack is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return head == null;
     }
